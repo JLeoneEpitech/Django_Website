bd0c4f18dafd4b78bd727eb525c8d64c0e0c9da6
Jonathan Leone <jonathan.leone@epitech.eu>
2024-10-03 15:52:33 +0200

    Adding some views

--- WebSite/polls/views.py
+++ WebSite/polls/views.py
@@ -1,6 +1,22 @@
-from django.shortcuts import HttpResponse
+from django.shortcuts import HttpResponse, render, get_object_or_404
+from django.template import loader
+from .models import Question
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Poll index here")
+    latest_question_list = Question.objects.order_by("-publication_date")[:5]
+    context = {"latest_question_list" : latest_question_list}
+    return render(request, "polls/index.html", context)
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html" , {"question" : question})
+
+def results(request, question_id):
+    response = "Result for question :  %s. "
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You are voting for question %s. " % question_id)
